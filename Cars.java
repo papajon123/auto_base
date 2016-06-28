736999fd2e44709e4b9685548dae41b17e9f02b1
nos_05@mail.ru <Zx123456>
2016-06-28 11:06:17 +0300

    add out

---
+++ Cars.java
@@ -0,0 +1,5 @@
+/**
+ * Created by Alex2 on 28.06.2016.
+ */
+public class Cars {
+}
---
+++ Programm.java
@@ -0,0 +1,10 @@
+/**
+ * Created by Alex2 on 27.06.2016.
+ */
+public class Programm {
+
+    public static void main(String[] args){
+
+        System.out.println("Hellow");
+    }
+}
